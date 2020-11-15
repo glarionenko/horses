37391b08327431313a88f8b529fb588abc5109a3
Georgii <glarionenko@gmail.com>
2020-11-15 18:12:55 +0300

    sdited template for Arduino

--- FeedingTemplate/FeedingTemplate.ino
+++ FeedingTemplate/FeedingTemplate.ino
@@ -4,16 +4,10 @@
 #define PWM_M 5
 #define UP_M A2
 #define DOWN_M A3
-#define END_SWITCH
+#define END_SWITCH 3
 
 boolean endSwitchStopValue = true;
 
-// Using the enum instruction allows for an easy method for adding and
-// removing registers. Doing it this way saves you #defining the size
-// of your slaves register array each time you want to add more registers
-// and at a glimpse informs you of your slaves register layout.
-
-//////////////// registers of your slave ///////////////////
 enum
 {
   // just add or remove registers and your good to go...
@@ -21,16 +15,18 @@ enum
   ENABLING,
   ROTATION,
   STATE_NOW,
+  LAST_COMMAND,
   HOLDING_REGS_SIZE // leave this one
   // total number of registers for function 3 and 16 share the same register array
   // i.e. the same address space
 };
 
 unsigned int holdingRegs[HOLDING_REGS_SIZE]; // function 3 and 16 register array
-////////////////////////////////////////////////////////////
 
 void setup()
 {
+  //объявить пин для датчика холла
+  pinMode(END_SWITCH, INPUT);
   pinMode(PWM_M, OUTPUT);
   pinMode(UP_M, OUTPUT);
   pinMode(DOWN_M, OUTPUT);
@@ -42,56 +38,68 @@ void setup()
   modbus_update_comms(4800, SERIAL_8N2, 2);
 
 }
-
-void loop()
-{
-  // modbus_update() is the only method used in loop(). It returns the total error
-  // count since the slave started. You don't have to use it but it's useful
-  // for fault finding by the modbus master.
-
-  modbus_update();
-
-
-
-  holdingRegs[ADC_VAL] = 0x1234; // update data to be read by the master to adjust the PWM
-
-  switch (holdingRegs[PWM_VAL]) {
-    case 0:
-      analogWrite(PWM_M, 0); // constrain adc value from the arduino master to 255
-      delay(1000);
-      digitalWrite(UP_M, 0); // constrain adc value from the arduino master to 255
-      digitalWrite(13, 0);
-      now_sp = 100;
+unsigned long motion_started = 0;
+int time_for_motion_down = 3000;
+int time_for_motion_up = 3000;
+boolean checkTime(unsigned long started, int timer) {
+  boolean ok = false;
+  if (millis() - started < timer) {
+    ok = true;
+  }
+  return ok;
+}
+boolean checkHall() {
+  //добавить повторную проверку
+  return digitalRead(END_SWITCH);
+}
+boolean motion_allowed(int dir) { //0 - down, 1 - up
+  boolean allowed = false;
+  switch (dir) {
+    case 2:
+      if (checkTime(motion_started, time_for_motion_down)) {
+        allowed = true;
+      }
       break;
     case 1:
-      if ((now_sp < 250) && (millis() - last_update > timer1)) {
-        now_sp++;
-        last_update = millis();
+      if (checkHall && checkTime(motion_started, time_for_motion_up)) {
+        allowed = true;
       }
-      analogWrite(PWM_M, now_sp); // constrain adc value from the arduino master to 255
-      digitalWrite(UP_M, 1); // constrain adc value from the arduino master to 255
+      break;
+  }
+  return allowed;
+}
+void loop()
+{
+  modbus_update();
+  switch (holdingRegs[ROTATION]) {
+    case 0:
+      analogWrite(PWM_M, 0);
+      delay(1000);//время на выключение питания
+      digitalWrite(UP_M, 0);
+      digitalWrite(DOWN_M, 0);
+      digitalWrite(13, 0);
+      //now_sp = 100;
+      break;
+    case 1:
+      //      if ((now_sp < 250) && (millis() - last_update > timer1)) {
+      //        now_sp++;
+      //        last_update = millis();
+      //      }
+      analogWrite(PWM_M, 250);//now_sp
+      delay(500);
+      digitalWrite(UP_M, 1);
       digitalWrite(13, 1);
       break;
     case 2:
-      if ((now_sp < 250) && (millis() - last_update > timer1)) {
-        now_sp++;
-        last_update = millis();
-      }
-      analogWrite(PWM_M, now_sp); // constrain adc value from the arduino master to 255
-      digitalWrite(DOWN_M, 1); // constrain adc value from the arduino master to 255
+      //      if ((now_sp < 250) && (millis() - last_update > timer1)) {
+      //        now_sp++;
+      //        last_update = millis();
+      //      }
+      analogWrite(PWM_M, 250);//now_sp
+      delay(500);
+      digitalWrite(DOWN_M, 1);
       digitalWrite(13, 1);
       break;
 
   }
-
-
-  /* Note:
-     The use of the enum instruction is not needed. You could set a maximum allowable
-     size for holdinRegs[] by defining HOLDING_REGS_SIZE using a constant and then access
-     holdingRegs[] by "Index" addressing.
-     I.e.
-     holdingRegs[0] = analogRead(A0);
-     analogWrite(LED, holdingRegs[1]/4);
-  */
-
 }
